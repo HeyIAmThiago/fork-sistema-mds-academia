01da5d8b3c8393824ff455b4f8abc3e79f369645
Entony Maxwell <94913785+entonymaxwell01@users.noreply.github.com>
2025-10-01 11:11:35 -0300

    chore: configurando container docker

--- back-end/startup/database.js
+++ back-end/startup/database.js
@@ -1,10 +1,16 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+const config = require("config");
 
 module.exports = function () {
-    const uri = process.env.DB_CONNECT_STRING;
-    mongoose.connect(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }, (e) => {e ? console.log(e) : {}});
-}
+  // Utiliza o pacote 'config' para obter a string de conexão,
+  // que é o padrão neste projeto.
+  const db = config.get("db");
+
+  // mongoose.connect agora retorna uma Promise.
+  mongoose
+    .connect(db)
+    .then(() => console.log(`✅ Conectado com sucesso ao MongoDB...`))
+    .catch((err) =>
+      console.error("❌ ERRO: Não foi possível conectar ao MongoDB.", err)
+    );
+};
